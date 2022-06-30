875f1a914874d94408a5e66b9df93454d283cf84
CoderRJohnson <93647481+CoderRJohnson@users.noreply.github.com>
2022-06-30 17:18:23 +0800

    Update introduction.tsx

--- packages/webapp/src/video-conferencing/introduction.tsx
+++ packages/webapp/src/video-conferencing/introduction.tsx
@@ -2,7 +2,7 @@ import { Link, Text } from "_app/ui";
 
 export const ZoomIntroduction = () => (
     <Text>
-        The EdenOS software integrates with{" "}
+        EOS Respect integrates with{" "}
         <Link href="https://zoom.us" target="_blank" isExternal>
             Zoom
         </Link>{" "}
