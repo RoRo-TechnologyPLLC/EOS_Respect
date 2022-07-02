386dc30cfc9dda3b224bf9849e67c2d5b25a0e03
CoderRJohnson <93647481+CoderRJohnson@users.noreply.github.com>
2022-07-02 14:09:26 +0800

    Update get-an-invite-cta.tsx

--- packages/webapp/src/inductions/components/get-an-invite-cta.tsx
+++ packages/webapp/src/inductions/components/get-an-invite-cta.tsx
@@ -6,12 +6,12 @@ export const GetAnInviteCTA = () => {
         <InductionStepsContainer step={InductionStepInvitee.GetInvite}>
             <>
                 <p className="mb-10 text-2xl font-medium title-font text-gray-900">
-                    Ready to join Eden? The membership process begins with an
+                    Ready to join EOS Respect? The membership process begins with an
                     invitation. Reach out to a current member to get yours!
                     We'll guide you through the rest.
                 </p>
                 <Button
-                    href="https://www.notion.so/edenos/Getting-an-Invite-2d38947d5be94dcb84dfa1ae48894802"
+                    href="https://www.notion.so/eoscommunity/EOS-Support-Squad-a22666b89d544be1a0b2f27eb06ec8ce"
                     size="lg"
                     target="_blank"
                     isExternal
